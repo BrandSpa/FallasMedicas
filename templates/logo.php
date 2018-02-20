6a6a3fc61838713834e7e8e4a5ef9279217cba6a
Michael Sanchez <michaelsanchez@MacBook-Air-de-Michael.local>
2018-02-20 11:37:37 -0500

    Change Logo Width and Height

--- templates/logo.php
+++ templates/logo.php
@@ -1,4 +1,4 @@
-<svg class='logo' width='185px' height='62' version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+<svg class='logo' width='159px' height='53' version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	 viewBox="0 0 159.7 53.4" style="enable-background:new 0 0 159.7 53.4;" xml:space="preserve">
 <style type="text/css">
 	.st0{fill:#FFFFFF;}
